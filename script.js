3000200f0491cb68265c8e1e8cbf77c32d27d3d2
Atrika Show <exyouknow15@gmail.com>
2023-02-15 00:13:21 +0530

    Live Code Editor ptoject commited

---
+++ script.js
@@ -0,0 +1,9 @@
+function run(){
+    const htmlCode = document.getElementById("html_code").value;
+    const cssCode = document.getElementById("css_code").value;
+    const jsCode = document.getElementById("js_code").value;
+    const output = document.getElementById("output");
+
+    output.contentDocument.body.innerHTML = htmlCode + "<style>"+ cssCode+"</style";
+    output.contentWindow.eval(jsCode);
+}
